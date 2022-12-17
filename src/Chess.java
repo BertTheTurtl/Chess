a7ab785c41544f10a24583cc3d203164b4d9a87c
BertTheTurtl <92934395+BertTheTurtl@users.noreply.github.com>
2022-12-17 03:59:58 -0500

    Doing a little bit of work on the board and have finished Piece.java

--- src/Chess.java
+++ src/Chess.java
@@ -11,15 +11,30 @@ public class Chess
         this.white = white;
         this.black = black;
         board = new Piece[][]{
-                {new Rook(w),new Knight(w),new Bishop(w),new Queen(w),new King(w),new Bishop(w),new Knight(w),new Rook(w)},
-                {new Pawn(w),new Pawn(w),new Pawn(w),new Pawn(w),new Pawn(w),new Pawn(w),new Pawn(w),new Pawn(w)},
+                {new Rook('w'),new Knight('w'),new Bishop('w'),new Queen('w'),new King('w'),new Bishop('w'),new Knight('w'),new Rook('w')},
+                {new Pawn('w'),new Pawn('w'),new Pawn('w'),new Pawn('w'),new Pawn('w'),new Pawn('w'),new Pawn('w'),new Pawn('w')},
                 {new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace()},
                 {new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace()},
                 {new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace()},
                 {new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace(),new EmptySpace()},
-                {new Pawn(b),new Pawn(b),new Pawn(b),new Pawn(b),new Pawn(b),new Pawn(b),new Pawn(b),new Pawn(b)},
-                {new Rook(b),new Knight(b),new Bishop(b),new Queen(b),new King(b),new Bishop(b),new Knight(b),new Rook(b)}
+                {new Pawn('b'),new Pawn('b'),new Pawn('b'),new Pawn('b'),new Pawn('b'),new Pawn('b'),new Pawn('b'),new Pawn('b')},
+                {new Rook('b'),new Knight('b'),new Bishop('b'),new Queen('b'),new King('b'),new Bishop('b'),new Knight('b'),new Rook('b')}
         };
+    }
+
+    public int countBlanks()
+    {
+
+    }
+
+    public char markerForPlayer(Player player)
+    {
+        if (player.equals(white))
+            return 'w';
+        else if (player.equals(black))
+            return 'b';
+        else
+            return '_';
     }
 
     public boolean checkWin(Player player)
@@ -30,6 +45,59 @@ public class Chess
         else
             enemy = white;
 
-        return /*!ENEMY'S KING.canMove()*/
+        return /*!ENEMY'S KING.canMove() && is in check*/
+    }
+
+    public boolean checkLose(Player player)
+    {
+        return /*player's king has no legal moves && is in check*/
+    }
+
+    public boolean checkDraw()
+    {
+        return !checkWin(white) && !checkWin(black) && /*either king has no legal moves and is not in check*/;
+    }
+
+    public ArrayList<Chess> possibleMoves(Player player)
+    {
+
+    }
+
+    //Getter methods
+    public Piece[][] getBoard()
+    {
+        return board;
+    }
+
+    public Player getWhite()
+    {
+        return white;
+    }
+
+    public Player getBlack()
+    {
+        return black;
+    }
+
+    //Setter methods
+    public void setBoard(Piece[][] newBoard)
+    {
+        board = newBoard;
+    }
+
+    public void setWhite(Player newWhite)
+    {
+        white = newWhite;
+    }
+
+    public void setBlack(Player newBlack)
+    {
+        black = newBlack;
+    }
+
+    //toString()
+    public String toString()
+    {
+
     }
 }
--- src/Piece.java
+++ src/Piece.java
@@ -5,9 +5,17 @@ public abstract class Piece
     private char color;
     private char type;
 
-    public abstract ArrayList<Chess> moves(Chess board, Piece piece);
+    public abstract ArrayList<Chess> moves(Chess board, int row, char column);
 
-    public abstract boolean canMove(Chess board, Piece piece, int row, char column);
+    public boolean canMove(Chess board, int row, char column)
+    {
+        return moves(board, row, column).size() > 0;
+    }
 
     public abstract boolean canEat(Chess board, Piece piece, int row, char column);
+
+    public String toString()
+    {
+        return "" +color +type;
+    }
 }
